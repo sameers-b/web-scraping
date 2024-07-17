ab9feefb0a1d92255716d3a10eb000dcf2a9676d
Sameer Singh <sameer@shadowcast.io>
2024-07-17 23:28:57 +0530

    new

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -11,8 +11,9 @@ const Header = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (isValidDomain(url)) createScrape({ url });
-    else toast.error("Invalid URL");
+    createScrape({ url });
+    // if (isValidDomain(url)) createScrape({ url });
+    // else toast.error("Invalid URL");
   };
 
   useEffect(() => {
@@ -53,10 +54,12 @@ const Header = () => {
             value={url}
             onChange={(e) => setUrl(e.target.value)}
             required
+            disabled={isLoading}
           />
           <button
             type="submit"
             className="text-[#6C2BD9]  bg-[#EDE5FF] hover:bg-purple-300 duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+            disabled={isLoading}
           >
             Fetch & Save Details
           </button>
