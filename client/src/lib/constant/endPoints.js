ad53a45e852a72d4daac3ed26a3b7c0e67039322
Sameer Singh <sameer@shadowcast.io>
2024-07-18 13:39:41 +0530

    new

--- client/src/lib/constant/endPoints.js
+++ client/src/lib/constant/endPoints.js
@@ -1,6 +1,6 @@
 export const BASE_URL_LOCAL = "http://localhost:8080/api/v1";
 export const BASE_URL_PRODUCTION =
-  "http://ec2-13-232-244-211.ap-south-1.compute.amazonaws.com:8080/api/v1";
+  "http://ec2-13-126-148-95.ap-south-1.compute.amazonaws.com:8080/api/v1";
 
 export const BASE_URL = BASE_URL_LOCAL;
 
